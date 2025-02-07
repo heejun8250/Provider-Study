993b43c30689fd9d7fb69141448ceea226ccfa2a
heejun <gmlwns262@naver.com>
2025-02-07 15:41:17 +0900

    Weather App - search까지

--- open_weather_provider/lib/pages/home_page.dart
+++ open_weather_provider/lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:open_weather_provider/providers/weather/weather_provider.dart';
+import 'package:open_weather_provider/pages/search_page.dart';
+import 'package:open_weather_provider/providers/providers.dart';
+import 'package:open_weather_provider/widgets/error_dialog.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,16 +12,63 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  String? _city;
+  late final WeatherProvider _weatherProv;
+
   @override
   void initState() {
     super.initState();
-    _fetchWeather();
+    _weatherProv = context.read<WeatherProvider>();
+    _weatherProv.addListener(_registerListener);
   }
 
-  _fetchWeather() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      context.read<WeatherProvider>().fetchWeather('london');
-    });
+  @override
+  void dispose() {
+    _weatherProv.removeListener(_registerListener);
+    super.dispose();
+  }
+
+  void _registerListener() {
+    final WeatherState ws = context.read<WeatherProvider>().state;
+
+    if (ws.status == WeatherStatus.error) {
+      errorDialog(context, ws.error.errMsg);
+    }
+  }
+
+  Widget _showWeather() {
+    final state = context.watch<WeatherProvider>().state;
+
+    if (state.status == WeatherStatus.initial) {
+      return Center(
+        child: Text(
+          '도시를 선택해 주세요.',
+          style: TextStyle(fontSize: 20),
+        ),
+      );
+    }
+
+    if (state.status == WeatherStatus.loading) {
+      return Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (state.status == WeatherStatus.error && state.weather.name == '') {
+      return Center(
+        child: Text(
+          '도시를 선택해 주세요.',
+          style: TextStyle(fontSize: 20),
+        ),
+      );
+    }
+
+    return Center(
+      child: Text(
+        state.weather.name,
+        style: TextStyle(fontSize: 18),
+      ),
+    );
   }
 
   @override
@@ -27,10 +76,26 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Weather'),
+        actions: [
+          IconButton(
+            onPressed: () async {
+              _city = await Navigator.push(context, MaterialPageRoute(
+                builder: (context) {
+                  return SearchPage();
+                },
+              ));
+
+              print('city: $_city');
+
+              if (_city != null) {
+                context.read<WeatherProvider>().fetchWeather(_city!);
+              }
+            },
+            icon: Icon(Icons.search),
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Home'),
-      ),
+      body: _showWeather(),
     );
   }
 }
--- open_weather_provider/lib/pages/search_page.dart
+++ open_weather_provider/lib/pages/search_page.dart
@@ -1,16 +1,75 @@
 import 'package:flutter/material.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final _formKey = GlobalKey<FormState>();
+  String? _city;
+  AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
+
+  void _submit() {
+    setState(() {
+      // 폼이 제출된 이후에는 글자마다 검증을 수행한다.
+      autovalidateMode = AutovalidateMode.always;
+    });
+
+    final form = _formKey.currentState;
+
+    if (form != null && form.validate()) {
+      form.save();
+      Navigator.pop(context, _city!.trim());
+    }
+  }
+
+  @override
+   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Search'),
       ),
-      body: Center(
-        child: Text('Search'),
+      body: Form(
+        key: _formKey,
+        autovalidateMode: autovalidateMode,
+        child: Column(
+          children: [
+            SizedBox(height: 60),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TextFormField(
+                autofocus: true,
+                style: TextStyle(fontSize: 18),
+                decoration: InputDecoration(
+                  labelText: '도시 이름',
+                  hintText: '2글자 이상',
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(),
+                ),
+                validator: (input) {
+                  if (input == null || input.trim().length < 2) {
+                    return '최소 2글자 이상 입력하세요';
+                  }
+                  return null;
+                },
+                onSaved: (input) {
+                  _city = input;
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submit,
+              child: Text(
+                '날씨 검색',
+                style: TextStyle(fontSize: 20),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
